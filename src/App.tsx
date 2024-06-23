1bb572859e72236257b6453610c6a82e613cd99e
jasooh <abuyuanjustin@gmail.com>
2024-06-23 03:44:44 -0400

    started Grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import Grid from "./components/Grid";
+
 function App() {
   return (
-    <div className="w-screen h-screen bg-slate-600 text-gray-300">rekt</div>
+    <div className="flex items-center justify-center w-screen h-screen bg-white">
+      <div className="p-10 w-2/3 h-2/3 rounded-xl border-4">
+        <Grid rows={5} columns={5} />
+      </div>
+    </div>
   );
 }
 
