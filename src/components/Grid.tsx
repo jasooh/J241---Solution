81d0f474b7b5d1bfc537b1ad2f298d7301b0a698
jasooh <abuyuanjustin@gmail.com>
2024-06-25 00:42:03 -0400

    added styling on controls

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -34,6 +34,18 @@ const Grid = ({rows, columns}:GridProps) => {
         return renderedComponents;
     }
 
+    // Setting row and column sizes
+    const gridStyle = {
+        gridTemplateRows: `repeat(${rows}, 1fr)`,
+        gridTemplateColumns: `repeat(${columns}, 1fr)`
+    }
+
+    // Reinitialize array when dimensions change
+    useEffect(() => {
+        setCellGrid(Array(rows*columns).fill(false));
+    }, [rows, columns])
+
+
     // Button logic
     const handleStartClick = () => {
         tick.startSimulation();
@@ -54,12 +66,6 @@ const Grid = ({rows, columns}:GridProps) => {
         tick.changeSimulationInterval(value);
     }
 
-    // Setting row and column sizes
-    const gridStyle = {
-        gridTemplateRows: `repeat(${rows}, 1fr)`,
-        gridTemplateColumns: `repeat(${columns}, 1fr)`
-    }
-
     // Status methods
     const getCellCount = (type: boolean) => {
         let count = 0;
@@ -72,10 +78,6 @@ const Grid = ({rows, columns}:GridProps) => {
         return count;
     }
     
-    useEffect(() => {
-        setCellGrid(Array(rows*columns).fill(false));
-    }, [rows, columns])
-
     return (
         <>
             <div className="flex flex-col items-center w-[60%] h-[90%] gap-3 shadow-xl p-5">
@@ -94,7 +96,7 @@ const Grid = ({rows, columns}:GridProps) => {
                 <div className="flex w-full h-[5rem] gap-2 justify-center">
                     <button className={`font-semibold rounded-md border p-2 hover:border-green-800 ${tick.isRunning.current ? 'border-green-800 text-green-800' : 'bg-white'} duration-200`} onClick={handleStartClick}>START</button>
                     <button className={`font-semibold rounded-md border p-2 ${!tick.isRunning.current ? 'border-yellow-800 text-yellow-800' : 'bg-white'} duration-200`} onClick={handlePauseClick}>PAUSE</button>
-                    <button className="font-semibold rounded-md border p-2 hover:border-red-800 hover:text-red-800" onClick={handleResetClick}>RESET</button>
+                    <button className="font-semibold rounded-md border p-2 hover:border-red-800 hover:text-red-800 duration-200" onClick={handleResetClick}>RESET</button>
                 </div>
 
                 {/* Time input */}
@@ -103,13 +105,14 @@ const Grid = ({rows, columns}:GridProps) => {
                     <input className="border border-black rounded w-30 py-1 px-2 mt-[-10px] mb-5 text-center" type="number" step="0.01" autoFocus={true} value={tick.interval} onChange={handleIntervalChange}/>
                 </div>
             </div>
+
             {/* Status tab */}
             <div className="flex flex-col gap-10 w-[20%] h-[50%] items-center shadow-xl p-10">
                 <label className="font-bold">STATUS WINDOW</label>
                 <div className="flex flex-col">
                     <label>Infected: {getCellCount(true)}</label>
                     <label>Normal: {getCellCount(false)} </label>
-                    <label>Generation: {tick.tick}</label>
+                    <label>Actions: {tick.tick}</label>
                 </div>
             </div>
         </>
